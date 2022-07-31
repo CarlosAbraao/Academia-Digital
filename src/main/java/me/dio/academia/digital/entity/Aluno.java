9667c41c710dda2478641b708e652d82ac39c135
Carlos Abraao <carloschavesnd@hotmail.com>
2022-07-31 15:55:29 -0300

    configuração do banco postgress e criação de tabelas ORM

--- src/main/java/me/dio/academia/digital/entity/Aluno.java
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -4,18 +4,50 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_alunos")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Aluno {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  
   private String nome;
 
+  @Column(unique = true)
   private String cpf;
 
   private String bairro;
 
   private LocalDate dataDeNascimento;
 
+  // A UM RELACIONAMENTO ENTRE ALUNO E AVALIAÇÃO FISICA
+  // UM ALUNO PODE TER VARIAS AVALIAÇÕES
+  @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY)
+  // PARA EVITAR UM POSSIVEL LOOP DE INFORMAÇÕES
+  @JsonIgnore
   private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
 
 }
--- src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
+++ src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -2,16 +2,46 @@ package me.dio.academia.digital.entity;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.ManyToAny;
+import org.hibernate.engine.internal.CascadePoint;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_avaliacoes")
 public class AvaliacaoFisica {
 
+	
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  // AS ALTERAÇÕES FEITAS NESSA TABELA AFETARÃO DIRETAMENTE A OUTRA
+  @ManyToOne(cascade = CascadeType.ALL)
+  // VAI CRIAR UM TABELA RELACIONANDO OS IDS
+  @JoinColumn(name = "aluno_id")
   private Aluno aluno;
 
   private LocalDateTime dataDaAvaliacao = LocalDateTime.now();
 
+  @Column(name = "peso_atual")
   private double peso;
-
+  @Column(name = "altura_atual")
   private double altura;
 
 }
