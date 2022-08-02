b49e01fb36b3a5d3840a161d7dfa19d87571f3e8
Carlos Abraao <carloschavesnd@hotmail.com>
2022-08-01 23:17:02 -0300

    criado consultas de avaliacoes e alunos

--- src/main/java/me/dio/academia/digital/service/impl/AlunoServiceImpl.java
+++ src/main/java/me/dio/academia/digital/service/impl/AlunoServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
 import me.dio.academia.digital.entity.form.AlunoForm;
 import me.dio.academia.digital.entity.form.AlunoUpdateForm;
 import me.dio.academia.digital.repository.AlunoRepository;
@@ -56,6 +57,12 @@ public class AlunoServiceImpl implements IAlunoService {
 	public void delete(Long id) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public List<AvaliacaoFisica> getAllAvaliacaoFisicaId(Long id) {
+		Aluno aluno = alunoRepository.findById(id).get();
+		return aluno.getAvaliacoes();
 	}
 
 }
